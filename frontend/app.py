15b53c696a1acab0d2656b50859f4d0763a71a24
Yvtx <110337875+yvtx@users.noreply.github.com>
2025-01-25 22:07:29 +0100

    [+] Titlebar in index.html/style.css

--- frontend/app.py
+++ frontend/app.py
@@ -1,12 +1,12 @@
 import webview
 
 def create_window():
-
     webview.create_window(
         title="WebGuard",
         url="src/index.html",
         width=800,
         height=600,
+        frameless=True
     )
     webview.start()
 
