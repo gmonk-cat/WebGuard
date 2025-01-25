fe789c5aee6449ea6956b2092c1b4102ba7f0014
Yvtx <110337875+yvtx@users.noreply.github.com>
2025-01-25 21:15:02 +0100

    Settings change app.py

--- frontend/app.py
+++ frontend/app.py
@@ -1,17 +1,12 @@
 import webview
 
 def create_window():
-    options = {
-        "frameless": True,
-        "resizable": True
-    }
 
     webview.create_window(
         title="WebGuard",
         url="src/index.html",
         width=800,
         height=600,
-        **options
     )
     webview.start()
 
